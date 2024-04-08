51a79048f071ecb6062a9a7450ee480c1abe1171
scott1028 <mic1028002@gmail.com>
2024-04-08 21:36:49 +0800

    update

--- web/src/components/Person/PeopleTreeCell/PersonEntry.jsx
+++ web/src/components/Person/PeopleTreeCell/PersonEntry.jsx
@@ -22,22 +22,39 @@ const PersonEntry = (props) => {
   const [isExpanded, setExpand] = useState(false)
   return (
     <li>
-      <span className="line"></span>
-      <span className="line-left"></span>
-      <span
-        className={`${viewDetail && viewDetail.id === id ? 'is-viewed' : ''}`}
-        onClick={() => {
-          onViewDetail(restProps)
-          setExpand((prevState) => !prevState)
-        }}
-        role="presentation"
-      >
-        <input type="checkbox" checked={isExpanded} disabled={!childrenItems?.length} />
-        {name ?? id}
-        <>({id})</>
-        {maried1 && <>, {maried1.name ?? m1}</>}
-        {maried2 && <>, {maried2.name ?? m2}</>}
-        {maried3 && <>, {maried3.name ?? m3}</>}{' '}
+      <span className="item-wrapper">
+        <span className="line"></span>
+        <span className="line-left"></span>
+        <span
+          className={`${viewDetail && viewDetail.id === id ? 'is-viewed' : ''}`}
+          onClick={() => {
+            if (!childrenItems?.length) {
+              return
+            }
+            setExpand((prevState) => !prevState)
+          }}
+          role="presentation"
+        >
+          <input
+            type="checkbox"
+            checked={isExpanded}
+            disabled={!childrenItems?.length}
+          />
+          {name ?? id}
+          <>({id})</>
+          {maried1 && <>, {maried1.name ?? m1}</>}
+          {maried2 && <>, {maried2.name ?? m2}</>}
+          {maried3 && <>, {maried3.name ?? m3}</>}
+        </span>
+        <button
+          className="detail-button"
+          onClick={(e) => {
+            e.stopPropagation()
+            onViewDetail(restProps)
+          }}
+        >
+          Detail
+        </button>
       </span>
       {isExpanded && childrenItems?.length > 0 && (
         <ul className="tree">
