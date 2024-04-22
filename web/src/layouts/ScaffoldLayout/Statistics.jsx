bb5ac984da65d4c8bee15816d9787f776a97b0c6
scott <scott.lan@appier.com>
2024-04-23 00:29:06 +0800

    update: statistics

--- web/src/layouts/ScaffoldLayout/Statistics.jsx
+++ web/src/layouts/ScaffoldLayout/Statistics.jsx
@@ -4,34 +4,33 @@ import { QUERY } from 'src/components/Person/PeopleTreeCell'
 
 const Statistics = () => {
   const { data: { people } = { people: [] } } = useQuery(QUERY)
-  let totalMYanPeople = 0
-  let totalFYanPeople = 0
-  let totalNonMYanPeople = 0
-  let totalNonFYanPeople = 0
+  let count_kind1 = 0
+  let count_kind2 = 0
+  let count_male = 0
+  let count_female = 0
+
   people.forEach((person) => {
     if (person.x2 === 1) {
+      count_kind1 += 1
       if (person.x6 === 1) {
-        totalMYanPeople += 1
+        count_male += 1
       } else if (person.x6 === 2) {
-        totalFYanPeople += 1
+        count_female += 1
       }
     } else if (person.x2 === 2) {
+      count_kind2 += 1
       if (person.x6 === 1) {
-        totalNonMYanPeople += 1
+        count_male += 1
       } else if (person.x6 === 2) {
-        totalNonFYanPeople += 1
+        count_female += 1
       }
     }
   })
+
   return (
     <>
       <span>
-        {[
-          totalMYanPeople,
-          totalFYanPeople,
-          totalNonMYanPeople,
-          totalNonFYanPeople,
-        ].join(',')}
+        {[count_kind1, count_kind2, count_male, count_female].join(', ')}
       </span>
     </>
   )
