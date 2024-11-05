1c36ae303d1072e9fdac9492ae974f4354c347b6
scott1028 <mic1028002@gmail.com>
2024-11-05 21:29:00 +0800

    fix: bugs

--- web/src/pages/PeopleReport1Page/PeopleReport1Page.jsx
+++ web/src/pages/PeopleReport1Page/PeopleReport1Page.jsx
@@ -1,45 +1,45 @@
 import { useQuery } from '@redwoodjs/web'
+
 import { QUERY } from 'src/components/Person/PeopleTreeCell'
 
 const PeopleReport1Page = () => {
   const { data: { people } = { people: [] } } = useQuery(QUERY)
-  const peopleMap = Object.fromEntries(people.map(person => [person.x1, person]));
+  const peopleMap = Object.fromEntries(
+    people.map((person) => [person.x1, person])
+  )
   const peopleX21 = people.filter((person) => person.p0)
 
-  let keyGEN = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
-  let peopleLink = [keyGEN,]
+  let keyGEN = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  let peopleLink = [keyGEN]
 
   keyGEN[0] = people[0]?.x1 ?? 0
   const peopleG01 = people.filter((person) => person.p0 === keyGEN[0])
   if (peopleG01.length === 0) peopleLink.push(keyGEN)
   let indexG01 = 0
-  while (indexG01 < peopleG01.length)
-  {
+  while (indexG01 < peopleG01.length) {
     keyGEN[1] = peopleG01[indexG01].x1 ?? 0
     const peopleG02 = people.filter((person) => person.p0 === keyGEN[1])
     if (peopleG02.length === 0) peopleLink.push(keyGEN)
     let indexG02 = 0
-    while (indexG02 < peopleG02.length)
-    {
+    while (indexG02 < peopleG02.length) {
       keyGEN[2] = peopleG02[indexG02].x1 ?? 0
       const peopleG03 = people.filter((person) => person.p0 === keyGEN[2])
       if (peopleG03.length === 0) peopleLink.push(keyGEN)
       let indexG03 = 0
-      while (indexG03 < peopleG03.length)
-      {
+      while (indexG03 < peopleG03.length) {
         keyGEN[3] = peopleG03[indexG03].x1 ?? 0
-        const peopleG04= people.filter((person) => person.p0 === keyGEN[3])
+        const peopleG04 = people.filter((person) => person.p0 === keyGEN[3])
         if (peopleG04.length === 0) peopleLink.push(keyGEN)
         let indexG04 = 0
-        while (indexG04 < peopleG04.length)
-        {
+        while (indexG04 < peopleG04.length) {
           keyGEN[4] = peopleG04[indexG04].x1 ?? 0
-          const peopleG05= people.filter((person) => person.p0 === keyGEN[4])
+          const peopleG05 = people.filter((person) => person.p0 === keyGEN[4])
           if (peopleG05.length === 0) peopleLink.push(keyGEN)
           let indexG05 = 0
-          while (indexG05 < peopleG05.length)
-          {
+          while (indexG05 < peopleG05.length) {
+            keyGEN[5] = peopleG05[indexG05].x1 ?? 0
             peopleLink.push(keyGEN)
+            keyGEN = [...keyGEN]
             indexG05++
           }
           indexG04++
@@ -69,29 +69,30 @@ const PeopleReport1Page = () => {
   // }
   // people.forEach(person => {
   // });
+  window.peopleLink = peopleLink
 
   return (
     <table border="1">
-        <thead>
-          <tr>
-            <td width="80"></td>
-            <td width="80">1世</td>
-            <td width="80">2世</td>
-            <td width="80">3世</td>
-            <td width="80">4世</td>
-            <td width="80">5世</td>
-            <td width="80">6世</td>
-            <td width="80">7世</td>
-            <td width="80">8世</td>
-            <td width="80">9世</td>
-            <td width="80">10世</td>
-            <td width="80">11世</td>
-            <td width="80">12世</td>
-          </tr>
-          <tr>{peopleLink.length}</tr>
-        </thead>
-        <tbody>
-{/*
+      <thead>
+        <tr>
+          <td width="80"></td>
+          <td width="80">1世</td>
+          <td width="80">2世</td>
+          <td width="80">3世</td>
+          <td width="80">4世</td>
+          <td width="80">5世</td>
+          <td width="80">6世</td>
+          <td width="80">7世</td>
+          <td width="80">8世</td>
+          <td width="80">9世</td>
+          <td width="80">10世</td>
+          <td width="80">11世</td>
+          <td width="80">12世</td>
+        </tr>
+        <tr>{peopleLink.length}</tr>
+      </thead>
+      <tbody>
+        {/*
           <tr>
             <td></td>
             <td>{peopleLink[0]?.keyGEN[1] ?? ''}</td>
@@ -134,7 +135,7 @@ const PeopleReport1Page = () => {
             <td>{peopleLink[0].keyGEN[5]}</td>
           </tr>
 */}
-        </tbody>
+      </tbody>
     </table>
   )
 }
